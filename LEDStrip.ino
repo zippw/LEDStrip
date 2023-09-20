e770dfd6e115649312c7ae021df8f718324f63f9
Mosunov Valeriy <zippwickness@gmail.com>
2023-09-20 17:58:25 +0300

    Update LEDStrip.ino

--- LEDStrip.ino
+++ LEDStrip.ino
@@ -72,61 +72,46 @@ void setup()
     Serial.print("Ada\n");
 }
 
-void loop()
-{
-    // Wait for first byte of Magic Word
-    for (i = 0; i < sizeof prefix; ++i)
-    {
-    waitLoop:
-        while (!Serial.available())
-            ;
-        ;
-        // Check next byte in Magic Word
-        if (prefix[i] == Serial.read())
-            continue;
-        // otherwise, start over
-        i = 0;
-        goto waitLoop;
-    }
-
-    // Hi, Lo, Checksum
-    while (!Serial.available())
-        ;
-    ;
-    hi = Serial.read();
-    while (!Serial.available())
-        ;
-    ;
-    lo = Serial.read();
-    while (!Serial.available())
-        ;
-    ;
-    chk = Serial.read();
-
-    // If checksum does not match go back to wait
-    if (chk != (hi ^ lo ^ 0x55))
-    {
-        i = 0;
-        goto waitLoop;
-    }
-
-    memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
-
-    for (uint8_t i = 0; i < NUM_LEDS; i++)
-    {
-        byte r, g, b;
-        while (!Serial.available())
-            ;
-        r = Serial.read();
-        while (!Serial.available())
-            ;
-        g = Serial.read();
-        while (!Serial.available())
-            ;
-        b = Serial.read();
-        leds[i].r = r;
-        leds[i].g = g;
-        leds[i].b = b;
-    };
-    FastLED.show();
+void loop() { 
+  // Wait for first byte of Magic Word
+  for(i = 0; i < sizeof prefix; ++i) {
+    waitLoop: while (!Serial.available()) ;;
+    // Check next byte in Magic Word
+    if(prefix[i] == Serial.read()) continue;
+    // otherwise, start over
+    i = 0;
+    goto waitLoop;
+  }
+  
+  // Hi, Lo, Checksum  
+  while (!Serial.available()) ;;
+  hi=Serial.read();
+  while (!Serial.available()) ;;
+  lo=Serial.read();
+  while (!Serial.available()) ;;
+  chk=Serial.read();
+  
+  // If checksum does not match go back to wait
+  if (chk != (hi ^ lo ^ 0x55)) {
+    i=0;
+    goto waitLoop;
+  }
+  
+  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+  // Read the transmission data and set LED values
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    byte r, g, b;    
+    while(!Serial.available());
+    r = Serial.read();
+    while(!Serial.available());
+    g = Serial.read();
+    while(!Serial.available());
+    b = Serial.read();
+    leds[i].r = r;
+    leds[i].g = g;
+    leds[i].b = b;
+  }
+  
+  // Shows new values
+  FastLED.show();
 }
